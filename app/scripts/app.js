815d4978b307aa34c3d995c5e3b41b8e626cc422
Martin Gauer <martingauer@Martins-MacBook-Pro.local>
2015-05-27 16:45:11 +0200

    add all screens

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,6 +28,30 @@ angular
       .when('/login', {
         templateUrl: 'views/login.html'
       })
+      .when('/welcome-1', {
+        templateUrl: 'views/welcome-1.html'
+      })
+      .when('/welcome-2', {
+        templateUrl: 'views/welcome-2.html'
+      })
+      .when('/welcome-3', {
+        templateUrl: 'views/welcome-3.html'
+      })
+      .when('/welcome-4', {
+        templateUrl: 'views/welcome-4.html'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html'
+      })
+      .when('/create-app-1', {
+        templateUrl: 'views/create-app-1.html'
+      })
+      .when('/create-app-2', {
+        templateUrl: 'views/create-app-2.html'
+      })
+      .when('/create-app-3', {
+        templateUrl: 'views/create-app-3.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
