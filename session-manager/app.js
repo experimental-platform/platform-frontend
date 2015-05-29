2830504fd1367c8d22de58d9607305a44a796af2
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-05-29 18:18:56 +0200

    Route corrected.

--- session-manager/app.js
+++ session-manager/app.js
@@ -19,9 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/protonet', express.static(path.join(__dirname, 'public')));
 
-app.use('/api', routes);
+app.use('/protonet/api', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
