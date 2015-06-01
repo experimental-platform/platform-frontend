abcf3b69160996f794241ea54829e13e95dc8141
Andreas Litt <andreas.litt@gmail.com>
2015-06-01 16:25:58 +0200

    use better documented bcrypt package + make hashing/checking synchronus

--- session-manager/routes/api.js
+++ session-manager/routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var password = require('bcrypt-password');
+var password = require('bcrypt-nodejs');
 var request = require('request');
 var HttpStatus = require('http-status-codes');
 
@@ -23,31 +23,27 @@ function error_helper(statusCode, message) {
 router.post('/login', function (req, res, next) {
     var secret = req.body['password'];
     if (secret == undefined || secret == "") {
-      res.json()
+      next(error_helper(HttpStatus.FORBIDDEN, 'No Password given.'))
     } else {
-      password.hash(secret, function (error, digest) {
-        request({
-          url: API_URL + '/password',
-          json: true
-        }, function (error, response, result) {
-            if (response.statusCode === HttpStatus.NOT_FOUND) {
-              next(error_helper(HttpStatus.NOT_FOUND, 'Password not set.'));
-            } else if (response.statusCode === HttpStatus.OK) {
-              var saved_digest = result['value'];
-
-              password.check(digest, saved_digest, function (error, match) {
-                if (match === true) {
-                  req.session.logged_in = true;
-                  res.json({status: "Ok"})
-                } else {
-                  // TODO: check for error?
-                  next(error_helper(HttpStatus.FORBIDDEN, 'Wrong Password, access denied.'));
-                }
-              });
+      request({
+        url: API_URL + '/password',
+        json: true
+      }, function (error, response, result) {
+          if (response.statusCode === HttpStatus.NOT_FOUND) {
+            next(error_helper(HttpStatus.NOT_FOUND, 'Password not set.'));
+          } else if (response.statusCode === HttpStatus.OK) {
+            var saved_digest = result['value'];
+            console.log(saved_digest);
+            console.log(secret)
+            if (password.compareSync(secret, saved_digest)) {
+              req.session.logged_in = true;
+              res.json({status: "Ok"})
+            } else {
+              next(error_helper(HttpStatus.FORBIDDEN, 'Wrong Password, access denied.'));
             }
           }
-        );
-      });
+        }
+      );
     }
   }
 );
@@ -66,22 +62,21 @@ router.post('/password', function (req, res, next) {
       // TODO: Add more Password requirments!
       // * Check if password was confirmed
       // * Check if old password is correct!
-      password.hash(secret, function (error, digest) {
-        var options = {
-          url: API_URL + '/password',
-          method: 'POST',
-          json: true,
-          form: {
-            value: digest
-          }
+      var digest = password.hashSync(secret);
+      var options = {
+        url: API_URL + '/password',
+        method: 'POST',
+        json: true,
+        form: {
+          value: digest
         }
-        request(options, function(err, response, result) {
-          if (response.statusCode === HttpStatus.OK) {
-            res.json({status: "Ok"})
-          } else {
-            next(error_helper(HttpStatus.INTERNAL_SERVER_ERROR, 'Password could not be set.'));
-          }
-        });
+      }
+      request(options, function(err, response, result) {
+        if (response.statusCode === HttpStatus.OK) {
+          res.json({status: "Ok"})
+        } else {
+          next(error_helper(HttpStatus.INTERNAL_SERVER_ERROR, 'Password could not be set.'));
+        }
       });
     } else {
       next(error_helper(HttpStatus.NOT_FOUND, 'No password given.'));
