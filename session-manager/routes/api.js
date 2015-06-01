ef3dbedbb08b57dfb7d2b9955761fd1a19557fd3
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-01 10:42:23 +0200

    Initial login route

--- session-manager/routes/api.js
+++ session-manager/routes/api.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var password = require('bcrypt-password');
+var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Protonet Session Manager!' });
-});
+router.post('/login', function (req, res, next) {
+    var secret = req.body['password'];
+    password.hash(secret, function (error, digest) {
+      request({url: 'http://127.0.0.1:8080/password', json: true}, function (error, response, body) {
+          if (response.statusCode === 404) {
+            // TODO: password not set
+            var err = new Error('Not Found');
+            err.status = 404;
+            err.message = 'Password not set.';
+            next(err);
+          } else if (response.statusCode === 200) {
+            var saved_digest = body['value'];
+
+            password.check(digest, saved_digest, function (error, match) {
+              if (match === true) {
+                res.send('OKAY');
+                // TODO: set cookie
+              } else {
+                // TODO: check for error?
+                var err = new Error('Not Found');
+                err.status = 403;
+                err.message = 'Wrong Password, access denied.';
+                next(err);
+              }
+            });
+          }
+        }
+      );
+    });
+  }
+);
+
+router.post('/logout', function (req, res, next) {
+    // TODO: destroy & disable cookie
+  }
+);
+
+
+router.post('/password', function (req, res, next) {
+    var password = req.body['password'];
+    // TODO: Set password
+  }
+);
+
 
 module.exports = router;
