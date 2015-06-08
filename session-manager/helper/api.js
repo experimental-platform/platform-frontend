91e27e8b690e65409db0a84d3a778e9f428a7d5e
Andreas Litt <andreas.litt@gmail.com>
2015-06-08 14:44:23 +0200

    add app-manager api helper

--- session-manager/helper/api.js
+++ session-manager/helper/api.js
@@ -1,13 +1,19 @@
 var urljoin = require('url-join');
 
 var SKVS_API_URL = "http://skvs"
+var APPS_API_URL = "http://app-manager"
 
 if (process.env.NODE_ENV == 'development') {
   SKVS_API_URL = "http://127.0.0.1:8080"
+  APPS_API_URL = "http://127.0.0.1:8081"
 }
 
 module.exports = {
   skvsApiUrl: function(path) {
     return urljoin(SKVS_API_URL, path);
+  }
+
+  appsApiUrl: function(path) {
+    return urljoin(APPS_API_URL, path)
   }
 }
