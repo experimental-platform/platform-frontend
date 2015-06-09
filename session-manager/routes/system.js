6700207e4f724c4840a2dfbbd5327fed32cde1b5
Andreas Litt <andreas.litt@gmail.com>
2015-06-09 17:39:40 +0200

    test again

--- session-manager/routes/system.js
+++ session-manager/routes/system.js
@@ -2,6 +2,6 @@ var auth = require('../helper/auth');
 
 module.exports = function(router) {
   router.get('/system', auth, function(req, res, next) {
-    res.json({ status: "ok" });
+    res.json({ status: "okay" });
   });
 }
