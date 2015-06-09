b4a1c28e491c98b854df124e1e7cf371739f1197
Andreas Litt <andreas.litt@gmail.com>
2015-06-09 17:00:17 +0200

    commit spam for testing :/

--- session-manager/routes/system.js
+++ session-manager/routes/system.js
@@ -2,6 +2,6 @@ var auth = require('../helper/auth');
 
 module.exports = function(router) {
   router.get('/system', auth, function(req, res, next) {
-    res.json({});
+    res.json({ status: "ok" });
   });
 }
