792e307179dd92d439dc64345f8ab80b83cc0830
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-10 12:19:36 +0200

    Comment, format.

--- session-manager/routes/system.js
+++ session-manager/routes/system.js
@@ -1,7 +1,8 @@
 var auth = require('../helper/auth');
 
 module.exports = function(router) {
+  // TODO: return system data: IP, hostname, hardware revision, software revision, etc.
   router.get('/system', auth, function(req, res, next) {
     res.json({ status: "okay" });
   });
-}
+};
