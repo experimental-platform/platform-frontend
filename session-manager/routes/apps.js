b5fd9327f9c9d6a32b31f03addc3473e74af2fb6
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-10 12:18:04 +0200

    Add `;`'s, remove unused funktion, rebuild bad spaces.

--- session-manager/routes/apps.js
+++ session-manager/routes/apps.js
@@ -1,24 +1,13 @@
-var request = require('request').defaults({ json: true });
+var request = require('request').defaults({json: true});
 var HttpStatus = require('http-status-codes');
-var auth = require('../helper/auth')
-var api = require('../helper/api').appsApiUrl
-var error_helper = require('../helper/error').errorHelper
-var request_handler = require('../helper/error').requestHandler
+var auth = require('../helper/auth');
+var api = require('../helper/api').appsApiUrl;
+var error_helper = require('../helper/error').errorHelper;
+var request_handler = require('../helper/error').requestHandler;
 
-function ensureAppNameGiven(cb) {
-  return function(req, res, next) {
-    var name = req.body['name'];
-    if(name != undefined && name != "") {
-      cb(name, req, res, next);
-    } else {
-      next(error_helper(HttpStatus.BAD_REQUEST));
-    }
-  }
-}
-
-module.exports = function(router) {
-  router.get('/apps', auth, function(req, res, next) {
-    request(api('/list'), request_handler(function(response, res_result) {
+module.exports = function (router) {
+  router.get('/apps', auth, function (req, res, next) {
+    request(api('/list'), request_handler(function (response, res_result) {
       if (response.statusCode == HttpStatus.OK) {
         res.json(res_result);
       } else {
@@ -28,20 +17,20 @@ module.exports = function(router) {
   });
 
   var valid_post_actions = ['start', 'stop', 'restart', 'destroy', 'rebuild'];
-  router.post('/apps/:action', auth, function(req, res, next) {
+  router.post('/apps/:action', auth, function (req, res, next) {
     var name = req.body['name'];
-    if(name != undefined && name != "") {
+    if (name != undefined && name != "") {
       var action = req.params["action"];
-      if(valid_post_actions.indexOf(action) != -1) {
+      if (valid_post_actions.indexOf(action) != -1) {
         var options = {
           url: api('/' + action),
           method: 'POST',
           form: {
             name: name
           }
-        }
-        request(options, request_handler(function(response, res_result) {
-          if(response.statusCode == HttpStatus.OK) {
+        };
+        request(options, request_handler(function (response, res_result) {
+          if (response.statusCode == HttpStatus.OK) {
             res.json(res_result);
           } else {
             next(error_helper(HttpStatus.NOT_FOUND));
@@ -56,12 +45,12 @@ module.exports = function(router) {
   });
 
   var valid_get_actions = ['urls', 'logs'];
-  router.get('/apps/:action/:appname', auth, function(req, res, next) {
+  router.get('/apps/:action/:appname', auth, function (req, res, next) {
     var action = req.params["action"];
     var appname = req.params["appname"];
-    if(valid_get_actions.indexOf(action) != -1) {
-      request(api('/' + action + '/' + appname), request_handler(function(response, res_result) {
-        if(response.statusCode == HttpStatus.OK) {
+    if (valid_get_actions.indexOf(action) != -1) {
+      request(api('/' + action + '/' + appname), request_handler(function (response, res_result) {
+        if (response.statusCode == HttpStatus.OK) {
           res.json(res_result);
         } else {
           next(error_helper(HttpStatus.NOT_FOUND));
@@ -71,4 +60,4 @@ module.exports = function(router) {
       next(error_helper(HttpStatus.BAD_REQUEST));
     }
   });
-}
+};
