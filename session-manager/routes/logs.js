3610d9ba04193ddd869430a4a7f4e4711b4ea204
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-10 17:42:07 +0200

    Initial request works.

--- session-manager/routes/logs.js
+++ session-manager/routes/logs.js
@@ -5,16 +5,18 @@ var error_helper = require('../helper/error').errorHelper;
 var request_handler = require('../helper/error').requestHandler;
 
 module.exports = function (router) {
-  //router.get('/logs/all', auth, function (req, res, next) {
-  //    var options = {
-  //      url: 'http://127.0.0.1:19531/entries?boot',
-  //      method: 'PUT',
-  //      headers: {
-  //        Accept: 'application/json'
-  //        // Range: 'entries=cursor:100'
-  //      }
-  //    };
-  //    request(options, request_handler())
-  //  }
-  //)
+  router.get('/logs/all', auth, function (req, res, next) {
+      var options = {
+        url: 'http://127.0.0.1:19531/entries?boot',
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json'
+          // Range: 'entries=cursor:100'
+        }
+      };
+      request(options, request_handler(function (response, result) {
+        response.json(result);
+      }, next))
+    }
+  )
 };
