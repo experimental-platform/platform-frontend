e68f90b3e66b46687fd2602413ea4bb0fbf850b7
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-12 15:05:12 +0200

    Show log – WIP part I

--- session-manager/routes/logs.js
+++ session-manager/routes/logs.js
@@ -4,19 +4,21 @@ var auth = require('../helper/auth');
 var error_helper = require('../helper/error').errorHelper;
 var request_handler = require('../helper/error').requestHandler;
 
+var exec = require('child_process').exec();
+
 module.exports = function (router) {
-  router.get('/logs/all', auth, function (req, res, next) {
-      var options = {
-        url: 'http://127.0.0.1:19531/entries?boot',
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json'
-          // Range: 'entries=cursor:100'
-        }
-      };
-      request(options, request_handler(function (response, result) {
-        response.json(result);
-      }, next))
+  router.get('/logs', auth, function (req, res, next) {
+      // 'journalctl --output=json --boot --utc --no-pager --unit=sshd.socket'
+      child = exec('journalctl --output=json --boot --utc --no-pager',
+        request_handler(function (error, stdout, stderr) {
+          if (error == null && stderr == '') {
+            res.json(stdout);
+          } else {
+            console.log('exec error: ' + error);
+            console.log('stderr: ' + stderr);
+            next(error_helper(HttpStatus.BAD_REQUEST));
+          }
+        }, next));
     }
   )
 };
