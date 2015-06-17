44e337b34b6d10b22744c4c7e702d362cd0e30e2
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-17 13:22:43 +0200

    Removed unneeded requires.

--- session-manager/routes/logs.js
+++ session-manager/routes/logs.js
@@ -1,9 +1,5 @@
-var request = require('request').defaults({json: true});
-
 var auth = require('../helper/auth');
-var error_helper = require('../helper/error').errorHelper;
 var run_cmd = require('../helper/cli').run_cmd;
-var exec = require('child_process').exec;
 
 // TODO: Remove this to reenable authentication!
 auth = function (req, res, next) {
