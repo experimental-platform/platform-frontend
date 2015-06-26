28d676b91c236c7343c657628659dac236107bb8
Andreas Litt <andreas.litt@gmail.com>
2015-06-26 12:03:29 +0200

    implement GET update mechanism

--- management-proxy/session-manager/helper/api.js
+++ management-proxy/session-manager/helper/api.js
@@ -2,7 +2,8 @@ var urljoin = require('url-join');
 
 var SKVS_API_URL = "http://skvs";
 var APPS_API_URL = "http://app-manager";
-var MONITORING_API_URL = "http://monitoring"
+var MONITORING_API_URL = "http://monitoring";
+var DOCKERHUB_API_URL = "https://index.docker.io/v1/repositories/experimentalplatform";
 
 if (process.env.NODE_ENV == 'development') {
   SKVS_API_URL = "http://127.0.0.1:8080";
@@ -16,10 +17,14 @@ module.exports = {
   },
 
   appsApiUrl: function(path) {
-    return urljoin(APPS_API_URL, path)
+    return urljoin(APPS_API_URL, path);
   },
 
   monitoringApiUrl: function(path) {
-    return urljoin(MONITORING_API_URL, path)
+    return urljoin(MONITORING_API_URL, path);
+  },
+
+  dockerHubApiUrl: function(path) {
+    return urljoin(DOCKERHUB_API_URL, path);
   }
 };
