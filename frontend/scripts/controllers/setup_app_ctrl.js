5a071ea05f05390cf186b8ef2e83a53ffc557ce6
Christopher Blum <christopher.blum@spellboy.com>
2015-08-03 14:28:53 +0200

    App tutorials: Show the hostname that the user entered in his address bar

--- frontend/scripts/controllers/setup_app_ctrl.js
+++ frontend/scripts/controllers/setup_app_ctrl.js
@@ -3,7 +3,12 @@ angular.module("protonet.platform").controller("SetupAppCtrl", function($scope, 
 
   $scope.docs = AppTutorial.get($scope.appType).setup;
 
+  var host = location.hostname;
+  if (host.indexOf(".protonet.info") !== -1) {
+    host = $scope.nodename + ".local";
+  }
+
   $scope.interpolate = function(str) {
-    return str.replace("{{host}}", $scope.nodename + ".local");
+    return str.replace("{{host}}", host);
   };
 });
