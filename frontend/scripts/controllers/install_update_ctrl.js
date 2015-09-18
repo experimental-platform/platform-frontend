784480ee0395bb5ba8a8eb50ee794ef27c50ab0a
Benjamin Behr <benny@digitalbehr.de>
2015-09-18 11:52:20 +0200

    add an option for a more detailed update status

--- frontend/scripts/controllers/install_update_ctrl.js
+++ frontend/scripts/controllers/install_update_ctrl.js
@@ -1,4 +1,25 @@
 angular.module("protonet.platform").controller("InstallUpdateCtrl", function($scope, $state, $timeout, API, Notification) {
+
+  function toggleDetailedUpdateStatus() {
+    $scope.showDetailedUpdateStatus = !$scope.showDetailedUpdateStatus;
+  }
+  $scope.toggleDetailedUpdateStatus = toggleDetailedUpdateStatus;
+
+  function updateDetailedStatus(images) {
+    $scope.status = {};
+    for (var property in images) {
+      if (images.hasOwnProperty(property)) {
+        var image = images[property];
+        $scope.status[property] = {
+          local: image.local,
+          remote: image.remote,
+          upToDate: image.local === image.remote
+        }
+      }
+    }
+    $scope.statusAvailable = !$.isEmptyObject($scope.status);
+  }
+
   function check() {
     $timeout(function() {
       API.get("/admin/api/system/update").then(function(data) {
@@ -6,6 +27,7 @@ angular.module("protonet.platform").controller("InstallUpdateCtrl", function($sc
           Notification.notice("Update successfully installed");
           $state.go("dashboard.index");
         } else {
+          updateDetailedStatus(data.images);
           check();
         }
       }).catch(check);
