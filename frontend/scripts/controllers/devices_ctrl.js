07f93316602251367b68ef7c1dbe4a2e59ba46e7
Christopher Blum <christopher.blum@spellboy.com>
2015-11-21 12:48:36 +0100

    First working version using a slightly fine tuned filter.

--- frontend/scripts/controllers/devices_ctrl.js
+++ frontend/scripts/controllers/devices_ctrl.js
@@ -1,53 +1,67 @@
-angular.module("protonet.platform").controller("DevicesCtrl", function($scope, $state, API, Device) {
-  // @Hinnerk comment this in as soon as you have a json api at /admin/api/devices
-  // Device.startFetcher();
-  // $scope.devices = Device.records;
-  // $scope.$on("$destroy", function() {
-  //   Device.stopFetcher();
-  // });
-  
-  // @Hinnerk, this is demo data:
-  $scope.devices = [{
-    path: "/dev/video0",
-    name: "D-Link Webcam",
-    manufacturer: "D-Link",
-    type: "video_input"
-  }, {
-    path: "/dev/snd/xxx",
-    name: "Foobar Mic",
-    manufacturer: "Foobar",
-    type: "audio_input"
-  }, {
-    path: "/dev/snd/xxx",
-    name: "Logitech Speakers",
-    manufacturer: "Logitech",
-    type: "audio_output"
-  }, {
-    path: "/dev/tty0",
-    name: "Unknown Serial Device",
-    manufacturer: "Unknown",
-    type: "usb_serial"
-  }];
+angular.module("protonet.platform").controller("DevicesCtrl", function ($scope, $state, API, Device) {
+  Device.startFetcher();
+  $scope.devices = Device.records;
+  $scope.$on("$destroy", function () {
+    Device.stopFetcher();
+  });
 
   var icons = {
     video_input: "images/hardware-icons/video_output.svg",
+    video4linux: "images/hardware-icons/video_output.svg",
     audio_output: "images/hardware-icons/audio_output.svg",
     audio_input: "images/hardware-icons/audio_input.svg",
-    usb_serial: "images/hardware-icons/usb_serial.svg"
+    sound: "images/hardware-icons/audio_output.svg",
+    usb_serial: "images/hardware-icons/usb_serial.svg",
+    tty: "images/hardware-icons/usb_serial.svg"
   };
 
   var examples = {
     video_input: "https://github.com/experimental-platform/example-webcam",
+    video4linux: "https://github.com/experimental-platform/example-webcam",
     audio_output: "https://github.com/experimental-platform/example-speaker",
     audio_input: "https://github.com/experimental-platform/example-voice-recognition",
-    usb_serial: "https://github.com/experimental-platform/radio-test"
+    sound: "https://github.com/experimental-platform/example-voice-recognition",
+    'usb-serial': "https://github.com/experimental-platform/radio-test",
+    tty: "https://github.com/experimental-platform/radio-test"
   };
 
-  $scope.getIcon = function(device) {
-    return icons[device.type];
+  var generic_description = {
+    tty: "Serial interface"
   };
 
-  $scope.getExample = function(device) {
-    return examples[device.type];
+  var getLastItem = function (liste) {
+    if (Object.prototype.toString.call(liste) === '[object Array]') {
+      return liste[liste.length - 1];
+    }
+    return liste;
+  };
+
+  $scope.getIcon = function (device) {
+    return icons[getLastItem(device.SUBSYSTEM)];
+  };
+
+  $scope.getName = function (device) {
+    var product = getLastItem(device.product),
+      serial = getLastItem(device.serial),
+      manufacturer = getLastItem(device.manufacturer),
+      devtype = getLastItem(device.SUBSYSTEM),
+      name = getLastItem(device.name),
+      result = '';
+    if (name) {
+      return name;
+    }
+    result += product ? product : '';
+    result += serial ? ' (SERIAL: ' + serial + ')' : '';
+    result += manufacturer ? ' by ' + manufacturer : '';
+    result = result ? result : generic_description[devtype];
+    return result;
+  };
+
+  $scope.getPath = function (device) {
+    return getLastItem(device.DEVNAME);
+  };
+
+  $scope.getExample = function (device) {
+    return examples[getLastItem(device.SUBSYSTEM)];
   };
 });
